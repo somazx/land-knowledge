e123adaa9369fc34f86e8fe6e50b4e0b9bae58a0
Andy Koch <somazx@gmail.com>
2022-07-03 13:25:05 -0700

    setting buildDir to dist
    Hopefully this allows vercel to deploy.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,8 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  buildDir: 'dist',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'land-knowledge',
